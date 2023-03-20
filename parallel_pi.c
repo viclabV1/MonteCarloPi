f345400482cd0bb66ddbdfcc7f5c9b83ecdac8bd
Victor LaBrie <vplabrie@gmail.com>
2023-03-20 17:51:11 -0500

    Parallelized using MPI.

--- parallel_pi.c
+++ parallel_pi.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <mpi.h>
+#include <mpi.h>
 #include <time.h>
 #include <math.h>
 
-void throws(long long n){
+long long throws(long long n, int rank){
     //seed rand()
     time_t t;
-    srand(&t);
-
+    srand(t+rank);
+    //making sure seed is unique
+    //printf("%ld ", rank+t);
     double x,y,distance_squared;
     long long in_circle=0;
     long long toss;
@@ -18,21 +19,37 @@ void throws(long long n){
         distance_squared = (x*x)+(y*y);
         if(distance_squared<=1) in_circle++;
     }
-    double pi_estimate = ((double)in_circle/(double)toss) * 4;
-    printf("%f", pi_estimate);
+    return in_circle;
+    //double pi_estimate = ((double)in_circle/(double)toss) * 4;
+    //printf("%f", pi_estimate);
 }
 
 int main(int argc, char *argv[]){
     int size, rank;
-    //MPI_INIT(&argc, &argv);
-    //MPI_Comm_size(&size, MPI_COMM_WORLD);
-
-    //Get number of 
+    MPI_Init(NULL,NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
+    
+    
+
     long long num_throws=strtoll(argv[1], NULL, 10);
-    
-    throws(num_throws);
+    long long num_throws_per=num_throws/size;
 
+    long long local_hits;
+    long long total_hits;
+    local_hits=throws(num_throws_per, rank);
+    //printf("%llu ", local_hits);
+    MPI_Reduce(&local_hits, &total_hits, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    if(rank==0){
+        //Was using to check if all local_hits were included in total_hits
+        //printf("%llu ", total_hits);
+        long double pi_estimate = ((long double)total_hits/(long double)num_throws)*4;
+        printf("%.7Lf\n", pi_estimate);
+    }
+    MPI_Finalize();
+    return 0;
 }
 
 
