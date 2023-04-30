2b7c8f2b3eb8d448fff82a851bce364781e8c048
viclabV1 <vplabrie@gmail.com>
2023-04-29 20:49:59 -0500

    Second version using semaphores instead of mutex

--- makefile
+++ makefile
@@ -1,7 +1,15 @@
 parallel_pi:
 	doxygen ./Doxyfile;
 	gcc -g -Wall -o parallel_pi parallel_pi.c -lm -lpthread
+	gcc -g -Wall -o no_mutex parallel_pi_no_mutex.c -lm -lpthread
+
+with_mutex:
+	gcc -g -Wall -o parallel_pi parallel_pi.c -lm -lpthread
+
+no_mutex:
+	gcc -g -Wall -o no_mutex parallel_pi_no_mutex.c -lm -lpthread
 
 clean:
+	rm ./no_mutex
 	rm ./parallel_pi
 	rm -rf ./doxygen_output
--- parallel_pi_no_mutex.c
+++ parallel_pi_no_mutex.c
@@ -13,6 +13,7 @@
 #include <time.h>
 #include <math.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 /**
  * @brief The actual Monte Carlo alogithm
@@ -31,8 +32,8 @@
 long long total_hits=0;
 long randOffset=0;
 pthread_mutex_t mutex;
-
-
+long long* resultArr;
+sem_t bar_semaphore;
 
 void* throws(void* n_p);
 
@@ -49,6 +50,7 @@ void* throws(void* n_p);
     long thread_count=strtol(argv[1],NULL,10);
     long long num_throws=strtoll(argv[2], NULL, 10);
     long long num_throws_per=num_throws/thread_count;
+    resultArr=malloc(sizeof(long long)*thread_count);
 
     //printf("%llu ", local_hits);
 
@@ -60,10 +62,15 @@ void* throws(void* n_p);
     
     pthread_mutex_init(&mutex,NULL);
     pthread_t* thread_arr = (pthread_t*) malloc (thread_count*sizeof(pthread_t));
-
-
+    //semaphores = malloc(sizeof(sem_t)*thread_count);
+    sem_init(&bar_semaphore,1,0);
     for(long thread=0; thread<thread_count; thread++){
         pthread_create(&thread_arr[thread], NULL, throws, (void*)num_throws_per);
+    }
+
+    for(long thread=0; thread<(thread_count);thread++){
+        //printf("Waiting\n");
+        sem_wait(&bar_semaphore);
     }
 
     for(long thread=0; thread<thread_count; thread++){
@@ -72,10 +79,17 @@ void* throws(void* n_p);
 
 
     pthread_mutex_destroy(&mutex);
+    
+    for(int i = 0; i<thread_count;i++){
+        //printf("%lld\n", resultArr[i]);
+        total_hits+=resultArr[i];
+    }
+
     long double pi_estimate = ((long double)total_hits/(long double)num_throws)*4;
     printf("%.15Lf\n", pi_estimate);
     printf("%f\n", (double)(time(NULL)-start));
     free(thread_arr);
+    free(resultArr);
     return 0;
     }
 
@@ -89,14 +103,14 @@ void* throws(void* n_p){
     */
     
     long long n = (long long)n_p;
-    
-    //unique rand seeds for each thread
+    //unique rand seeds for each thread, still using mutex for this
     time_t t = time(NULL);
     pthread_mutex_lock(&mutex);
+    long my_thread=randOffset;
     srand(t+randOffset);
     randOffset++;
     pthread_mutex_unlock(&mutex);
-    
+    //printf("%ld \n", randOffset);
 
     /**
      * @brief Monte Carlo algorithm
@@ -120,11 +134,11 @@ void* throws(void* n_p){
         distance_squared = (x*x)+(y*y);
         if(distance_squared<=1) in_circle++;
     }
-  
-    pthread_mutex_lock(&mutex);
-    total_hits += in_circle;
-    pthread_mutex_unlock(&mutex);
+    
 
+    resultArr[my_thread] = in_circle;
+    //printf("sem_post %lld, %ld\n",resultArr[my_thread], my_thread);
+    sem_post(&bar_semaphore);
     return NULL;
     //double pi_estimate = ((double)in_circle/(double)toss) * 4;
     //printf("%f", pi_estimate);
